e8b7bf14705e39a89c2b0e3c72731517ba8422a8
Antonio Parata <aparata@gmail.com>
2018-08-29 17:35:05 +0200

    implemented call instruction

--- SacaraSln/SacaraVm/vm_call.asm
+++ SacaraSln/SacaraVm/vm_call.asm
@@ -3,33 +3,41 @@ vm_call PROC
 	push ebp
 	mov ebp, esp	
 
-	; allocate space for the stack
-	push vm_stack_size
-	call heap_alloc
-	push eax
-
-	; get current stack frame
-	mov ebx, [ebp+arg0]
-	mov ebx, [ebx+vm_sp]
-
-	; init stack frame
-	push ebx
-	push eax
-	call vm_init_stack_frame
-
-	; set the new stack frame as the current one
-	pop eax
-	mov ebx, [ebp+arg0]
-	mov [ebx+vm_sp], eax
+	sub esp, 8
 
 	; pop the offset to call
 	push [ebp+arg0]
 	call vm_stack_pop
+	mov [ebp+local0], eax
+
+	; save on top of the current stack frame the ip
+	mov eax, [ebp+arg0]	
+	push [eax+vm_ip]
+	push [ebp+arg0]
+	call vm_stack_push
+
+	; allocate space for the stack
+	push vm_stack_size
+	call heap_alloc
+	mov [ebp+local1], eax
+
+	; init stack frame
+	mov ebx, [ebp+arg0]	
+	push [ebx+vm_sp] ; previous stack frame
+	push eax ; new allocated stack frame
+	call vm_init_stack_frame
+
+	; set the new stack frame as the current one
+	mov eax, [ebp+local1]
+	mov ebx, [ebp+arg0]
+	mov [ebx+vm_sp], eax
 		
 	; move the sp to the specific offset
-	mov ebx, [ebp+arg0]
-	mov [ebp+vm_ip], eax
+	mov ebx, [ebp+local0]
+	mov eax, [ebp+arg0]
+	mov [eax+vm_ip], ebx
 	
+	add esp, 8
 	mov ebp, esp
 	pop ebp
 	ret
