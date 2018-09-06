690c5f63191afbd1645f868457b2f02c62c52e0f
Antonio Parata <aparata@gmail.com>
2018-09-06 10:21:42 +0200

    Added more pretty printing for C language

--- SacaraSln/Sacara/Program.fs
+++ SacaraSln/Sacara/Program.fs
@@ -1,42 +1,12 @@
 ﻿namespace ES.Sacara
 
 open System
+open System.Text
 open ES.Sacara.Ir.Assembler
 
 module Program =
-    [<EntryPoint>]
-    let main argv = 
-        let irCode = """
-        proc main  
-            push start_code
-            call
-            halt
-        data:
-            BYTE "Ciao a tutti!!",0
-            WORD 0x1234,0x5678
-            DWORD 0x12345678
-        endp
-
-        proc start_code
-            push data
-            pop data_var
-        data2:
-            getip
-            pop local0
-            getsp
-            pop local1
-            ret
-        endp
-        """
-        let assembler = new SacaraAssembler()
-        assembler.Settings.RandomlyEncryptOpCode <- true
-        let code = assembler.Assemble(irCode)
-
-        let vmListing = code.ToString()        
-        Console.WriteLine(vmListing)
-        Console.WriteLine()
-
-        Console.WriteLine("MASM:")
+    let getMasmCode(code: IrAssemblyCode) =
+        let sb = new StringBuilder()
         let mutable index = 0
         code.Functions
         |> Seq.iter(fun irFunction ->
@@ -53,8 +23,65 @@ module Program =
                         then "0" + b
                         else b
                     )
-                Console.WriteLine("code_{0} BYTE {1} ; {2}", index.ToString("0000"), String.Join(",", bytesTring).PadRight(40), opCode.ToString())
+                sb.AppendFormat("code_{0} BYTE {1} ; {2}", index.ToString("0000"), String.Join(",", bytesTring).PadRight(40), opCode.ToString()).AppendLine() |> ignore
                 index <- index + 1
             )
         )
+        sb.ToString()
+
+    let getCCode(code: IrAssemblyCode) =
+        let sb = new StringBuilder("uint8_t code[] = {")
+        sb.AppendLine() |> ignore
+
+        let allOpCodes =
+            code.Functions
+            |> Seq.map(fun irFunction -> irFunction.Body)
+            |> Seq.concat
+            |> Seq.toList
+
+        allOpCodes
+        |> Seq.iteri(fun i opCode ->
+            let bytesTring = 
+                opCode.Buffer
+                |> Seq.map(fun b -> "0x" + b.ToString("X"))
+
+            let bytes = 
+                if i = allOpCodes.Length - 1
+                then String.Join(",", bytesTring)
+                else String.Join(",", bytesTring) + ","
+
+            sb.AppendFormat("\t{0} // {1}", bytes.PadRight(40), opCode.ToString()).AppendLine() |> ignore
+        )
+        sb.Append("};") |> ignore
+
+        sb.ToString()
+
+    [<EntryPoint>]
+    let main argv = 
+        let irCode = """
+        proc main  
+            push 12
+            push 44
+            cmp
+            push first_operand_is_less
+            jumpifl                     /* jump if 44 < 12 */
+            push 1
+            pop result_var
+        first_operand_is_less:
+            push 0
+            pop result_var
+            halt
+        endp
+        """
+        let assembler = new SacaraAssembler()
+        assembler.Settings.RandomlyEncryptOpCode <- false
+        let code = assembler.Assemble(irCode)
+
+        let vmListing = code.ToString()
+
+        Console.WriteLine("MASM:")
+        Console.WriteLine(getMasmCode(code))
+
+        Console.WriteLine("C:")
+        Console.WriteLine(getCCode(code))
         0
