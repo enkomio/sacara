b033142ee46e694b12e5310ae1446875f55a5d93
Antonio Parata <aparata@gmail.com>
2019-03-08 20:48:26 +0100

    Created EndToEndTests project

---
+++ Src/EndToEndTests/AssemblerTests.fs
@@ -0,0 +1,28 @@
+﻿namespace Sacara.EndToEndTests
+
+open System
+open ES.Sacara.Ir.Assembler
+open ES.Sacara.Ir.Core
+
+module AssemblerTests =
+
+    let private assembleCode(code: String) =
+        let assembler = new SacaraAssembler()
+        let vmCode = assembler.Assemble(code)
+        vmCode.Functions.[0].Body
+
+    let private assembleSingleInstruction(instructionText: String, irInstruction: IrInstruction, vmInstruction: VmInstruction) =
+        String.Format("""
+            proc main
+                {0}
+            endp
+        """, instructionText)
+        |> assembleCode
+        |> fun instructions ->
+            assert(instructions.Length = 1)
+            assert(instructions.[0].IrOp.Type = irInstruction)
+            assert(instructions.[0].Type = vmInstruction)
+
+    let ``Assemble halt - default settings``() =
+        assembleSingleInstruction("halt", IrInstruction.Halt, VmInstruction.VmHalt)
+
---
+++ Src/EndToEndTests/Program.fs
@@ -0,0 +1,9 @@
+﻿namespace Sacara.EndToEndTests
+
+open System
+
+module Program =
+    [<EntryPoint>]
+    let main argv = 
+        AssemblerTests.``Assemble text - default settings``()
+        0
