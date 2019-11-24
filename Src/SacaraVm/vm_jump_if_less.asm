467fac8248334305313fb92ca94a819bfc55e670
Antonio Parata <aparata@gmail.com>
2019-11-24 20:00:34 +0100

    Improved code

--- Src/SacaraVm/vm_jump_if_less.asm
+++ Src/SacaraVm/vm_jump_if_less.asm
@@ -7,11 +7,18 @@ vm_jump_if_less PROC
 	push [ebp+arg0]
 	call_vm_stack_pop_enc
 
-	; read the carry flag
+	; read the flags
 	mov ebx, [ebp+arg0]
 	mov ebx, (VmContext PTR [ebx]).flags
-	test ebx, VM_CARRY_FLAG
-	jz finish
+
+	; test relevant flags, see: http://faydoc.tripod.com/cpu/jl.htm
+	xor edx, edx
+	or edx, SET_VM_SIGN_FLAG
+	or edx, SET_VM_OVERFLOW_FLAG
+	and edx, ebx
+	popcnt edx, edx
+	cmp edx, 1
+	jnz finish	
 
 	; modify the vm IP
 	mov ebx, [ebp+arg0]
