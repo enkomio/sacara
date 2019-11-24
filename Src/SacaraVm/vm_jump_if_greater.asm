9f86f961e2553fffafc225dffae677a082940a3b
Antonio Parata <aparata@gmail.com>
2019-11-24 20:00:55 +0100

    Improved code

--- Src/SacaraVm/vm_jump_if_greater.asm
+++ Src/SacaraVm/vm_jump_if_greater.asm
@@ -7,12 +7,23 @@ vm_jump_if_greater PROC
 	push [ebp+arg0]
 	call_vm_stack_pop_enc
 
-	; read the carry flag
+	; read the flags
 	mov ebx, [ebp+arg0]
-	mov ebx, (VmContext PTR [ebx]).flags
-	test ebx, VM_CARRY_FLAG
+	mov ebx, (VmContext PTR [ebx]).flags	
+	
+	; test relevant flags, see: http://faydoc.tripod.com/cpu/jg.htm
+	test ebx, SET_VM_ZERO_FLAG
+	jz finish
+	
+	xor edx, edx
+	or edx, SET_VM_SIGN_FLAG
+	or edx, SET_VM_OVERFLOW_FLAG
+	and edx, ebx
+	popcnt edx, edx
+	cmp edx, 2
 	jnz finish
-
+	
+modify_ip:
 	; modify the vm IP
 	mov ebx, [ebp+arg0]
 	mov (VmContext PTR [ebx]).ip, eax
