32d777de27b1082daa970d3103e33df6eaa8fd78
Antonio Parata <aparata@gmail.com>
2019-11-29 10:17:29 +0100

    modified read instruction to accept type

--- Src/SacaraVm/vm_read.asm
+++ Src/SacaraVm/vm_read.asm
@@ -6,13 +6,32 @@ vm_read PROC
 	; pop the offset that we want to read
 	push [ebp+arg0]
 	call_vm_stack_pop_enc
-	
+
 	; read opcode
 	mov ebx, [ebp+arg0]
 	mov ebx, (VmContext PTR [ebx]).code
-	add eax, ebx
-	movzx eax, byte ptr [eax]
+	add ebx, eax
+	push ebx
+
+	; pop the offset type that we want to read
+	push [ebp+arg0]
+	call_vm_stack_pop_enc
+	pop ebx
 	
+	; read the value according to type
+	cmp eax, 3
+	jz dword_type
+	cmp eax, 2	
+	jz word_type
+	movzx eax, byte ptr [ebx]
+	jmp push_value
+word_type:
+	movzx eax, word ptr [ebx]
+	jmp push_value
+dword_type:
+	mov eax, dword ptr [ebx]
+
+push_value:
 	; push the value on top of the stack
 	push eax
 	push [ebp+arg0]
