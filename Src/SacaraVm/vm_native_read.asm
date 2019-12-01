7d775b2ef0002e76bc5670886262aa3b6b4fe517
Antonio Parata <aparata@gmail.com>
2019-12-01 14:24:24 +0100

    nread

--- Src/SacaraVm/vm_native_read.asm
+++ Src/SacaraVm/vm_native_read.asm
@@ -6,10 +6,27 @@ vm_native_read PROC
 	; pop the address that we want to read from native memory
 	push [ebp+arg0]
 	call_vm_stack_pop_enc
-	
-	; read byte
-	movzx eax, byte ptr [eax]
-	
+	push eax
+
+	; pop the offset type that we want to read
+	push [ebp+arg0]
+	call_vm_stack_pop_enc
+	pop esi
+
+	; read the value according to type
+	cmp eax, 3
+	jz dword_type
+	cmp eax, 2	
+	jz word_type
+	movzx eax, byte ptr [esi]
+	jmp push_value
+word_type:
+	movzx eax, word ptr [esi]
+	jmp push_value
+dword_type:
+	mov eax, dword ptr [esi]
+		
+push_value:
 	; push the value on top of the stack
 	push eax
 	push [ebp+arg0]
