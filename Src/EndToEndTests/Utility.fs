51888f0393bf545975f6744bf2b5ab3bece2bc63
Antonio Parata <aparata@gmail.com>
2019-12-01 14:27:57 +0100

    added more utils methods

--- Src/EndToEndTests/Utility.fs
+++ Src/EndToEndTests/Utility.fs
@@ -7,28 +7,52 @@ open ES.Sacara.Ir.Assembler
 open ES.Sacara.Ir.Core
 open ES.SacaraVm
 
+[<AutoOpen>]
 module internal Utility =
-    let _curDir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)
+    let scriptDir = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "TestSources")
     let assemblerSettings = new AssemblerSettings()    
 
-    let private getScriptSrc(scriptFile: String) =        
-        let scriptPath = Path.Combine(_curDir, "TestSources", scriptFile)
-        File.ReadAllText(scriptPath)
+    let getScriptFullPath(scriptFile: String) =
+        let selfContainedScript = Path.Combine(scriptDir, "SelfContained", scriptFile)
+        if File.Exists(selfContainedScript) then selfContainedScript
+        else Path.Combine(scriptDir, "Custom", scriptFile)
+
+    let getScriptSrc(scriptFile: String) = 
+        File.ReadAllText(getScriptFullPath(scriptFile))
 
     let private getSacaraVmDll() =
         // check if it was specified an input SacaraDll
         let args = Environment.GetCommandLineArgs()
-        if args.Length > 1
-        then args.[1]
-        else Path.Combine("..", "..", "..", "Debug", "SacaraVm.dll")
+        if args.Length > 1 then args.[1]
+        else 
+            let dllName = "SacaraVm.dll"
+            let debugPath = Path.Combine("..", "..", "..", "Debug", dllName)
+            if not <| File.Exists(debugPath) then
+                let releasePath = Path.Combine("..", "..", "..", "Release", dllName)
+                if not <| File.Exists(releasePath) then
+                    failwith "Unable to find the SacarVm Dll in the Debug or Release path. Please first compile it."
+                else
+                    releasePath
+            else
+                debugPath
 
     let private assembleCode(code: String) =        
         let assembler = new SacaraAssembler(assemblerSettings)
         let vmCode = assembler.Assemble(code)
         vmCode.Functions.[0].Body
 
+    let assertTest(testResult: Int32, expectedResult: Int32) =
+        if testResult <> expectedResult then
+            failwith(
+                String.Format(
+                    "Test returned value {0} instead of the expectedvalue {1}",
+                    testResult, 
+                    expectedResult
+                )
+            )
+
     let executeScriptWithArg(scriptFile: String, args: Int32 array) =
-        Console.WriteLine("Execute script: {0}", scriptFile)
+        Console.WriteLine("Execute script: {0}", scriptFile)        
         let sacaraVm = new SacaraVm(getSacaraVmDll())
 
         // assembly source
@@ -36,14 +60,11 @@ module internal Utility =
         let assembler = new SacaraAssembler(assemblerSettings)
         let vmCode = assembler.Assemble(script)
 
-        // set arguments
-        args
-        |> Array.iteri(fun i arg ->
-            sacaraVm.LocalVarSet(i, arg)
-        )
+        // print code for debugging
+        Console.WriteLine(vmCode)
 
         // run code and get result
-        sacaraVm.Run(vmCode)
+        sacaraVm.Run(vmCode, args)
         int32 <| sacaraVm.LocalVarGet(args.Length)
 
     let executeScript(scriptFile: String) =
@@ -68,3 +89,9 @@ module internal Utility =
     let assembleInstruction(irInstruction: IrInstruction, vmInstruction: VmInstruction) =
         assembleInstructionWithArg(irInstruction, vmInstruction, String.Empty)
 
+    let runTests(pattern: String) =
+        Assembly.GetExecutingAssembly().GetTypes()
+        |> Seq.find(fun t -> t.Name.EndsWith(pattern))
+        |> fun t -> t.GetMethods()
+        |> Seq.filter(fun m -> m.Name.StartsWith("Test"))
+        |> Seq.iter(fun m -> m.Invoke(null, Array.empty) |> ignore)
