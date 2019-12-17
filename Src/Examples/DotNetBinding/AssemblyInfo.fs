eb49a7687076ebfaf5a61ac863b118234bc975b3
Antonio Parata <aparata@gmail.com>
2019-12-17 12:25:54 +0100

    Bump version

--- Src/Examples/DotNetBinding/AssemblyInfo.fs
+++ Src/Examples/DotNetBinding/AssemblyInfo.fs
@@ -6,10 +6,10 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Sacara")>]
 [<assembly: AssemblyCompanyAttribute("Enkomio")>]
 [<assembly: AssemblyDescriptionAttribute("A VM stack based IR language well suite for code protection.")>]
-[<assembly: AssemblyVersionAttribute("2.3.485.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.3.485.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.3.485.0")>]
-[<assembly: AssemblyMetadataAttribute("BuildDate","2019-12-03")>]
+[<assembly: AssemblyVersionAttribute("2.4.485.14")>]
+[<assembly: AssemblyFileVersionAttribute("2.4.485.14")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.4.485.14")>]
+[<assembly: AssemblyMetadataAttribute("BuildDate","2019-12-17")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -17,7 +17,7 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "Sacara"
     let [<Literal>] AssemblyCompany = "Enkomio"
     let [<Literal>] AssemblyDescription = "A VM stack based IR language well suite for code protection."
-    let [<Literal>] AssemblyVersion = "2.3.485.0"
-    let [<Literal>] AssemblyFileVersion = "2.3.485.0"
-    let [<Literal>] AssemblyInformationalVersion = "2.3.485.0"
-    let [<Literal>] AssemblyMetadata_BuildDate = "2019-12-03"
+    let [<Literal>] AssemblyVersion = "2.4.485.14"
+    let [<Literal>] AssemblyFileVersion = "2.4.485.14"
+    let [<Literal>] AssemblyInformationalVersion = "2.4.485.14"
+    let [<Literal>] AssemblyMetadata_BuildDate = "2019-12-17"
